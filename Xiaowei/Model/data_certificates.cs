3a2772dfc0d58acbf538d2c3bf63cf4712a0aa79
wuling <onsuper@qq.com>
2019-02-01 10:57:07 +0800

    添加项目文件。

---
+++ Xiaowei/Model/data_certificates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiaowei.Model
+{
+    public abstract class result
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+
+    public class data_certificates//: result
+    {
+        public List<data_certificate> data { get; set; }
+    }
+
+    public class data_certificate
+    {
+        public string serial_no { get; set; }
+        public string effective_time { get; set; }
+        public string expire_time { get; set; }
+        public encrypt_certificate encrypt_certificate { get; set; }
+
+    }
+
+    public class encrypt_certificate
+    {
+        public string algorithm { get; set; }
+        public string nonce { get; set; }
+        public string associated_data { get; set; }
+        public string ciphertext { get; set; }
+    }
+}
